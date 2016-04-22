956bd57ad4c7ad59008869ce66723bfdf680c7d0
Remi Rampin <remirampin@gmail.com>
2016-04-22 17:31:52 -0400

    Optionally dump downloaded images

--- twitter_colormap/main.py
+++ twitter_colormap/main.py
@@ -14,12 +14,26 @@ from twitter_colormap.twitter_stream import get_twitter_statuses, post
 logger = logging.getLogger('twitter')
 
 
+DUMP = False
+
+
 def handle_status(status, images):
     viridis = jet = False
 
     for image_url in images:
         try:
             req = requests.get(image_url)
+            if DUMP:
+                if image_url.lower().endswith('.jpg'):
+                    ext = 'jpg'
+                else:
+                    ext = 'png'
+                fname = 'dumps/%s-%s.%s' % (
+                    status.created_at.strftime('%Y%m%d_%H%M%S'),
+                    status.author.screen_name,
+                    ext)
+                with open(fname, 'w') as fp:
+                    fp.write(req.content)
             image = Image.open(StringIO(req.content))
         except Exception as e:
             logger.exception("Exception retrieving image: %s", image_url)
